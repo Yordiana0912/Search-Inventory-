7fdd6bd2eb9c0697339f0451a83c527812cfce8a
Yordiana0912 <Yordiana0912@gmail.com>
2024-04-25 13:30:19 -0400

    completed

--- src/main/java/com/pluralsight/SearchInventory.java
+++ src/main/java/com/pluralsight/SearchInventory.java
@@ -1,14 +1,9 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SearchInventory {
-    public class ArrayListapp{
-        public static void main(String[] args) {
-            Array
-        }
-
-    }
     public static void main(String[] args) {
 
         ArrayList<Product> inventory = getInventory();
@@ -21,10 +16,16 @@ public class SearchInventory {
                     p.getId(), p.getName(), p.getPrice());
         }
     }
-    public ArrayList<Product> getInventory() {
+    public static ArrayList <Product> getInventory() {
         ArrayList<Product> inventory = new ArrayList<Product>();
         // this method loads product objects into inventory
         // and its details are not shown
+        inventory.add(new Product(1,"apples",1.50));
+        inventory.add(new Product (2,"oranges", 2.99));
+        inventory.add(new Product(3, "strawberry", 2.50));
+        inventory.add(new Product(4,"kiwi", 3.50));
+        inventory.add(new Product(5,"banana", 4.50));
         return inventory;
+
     }
 }
